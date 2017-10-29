213d175e84b3793586ec0e2b80a5be79c226dc51
scong-Liu <scongliu777@gmail.com>
2017-10-29 14:50:11 +0800

    1

---
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnOpenGL
+//
+//  Created by Apple on 2017/10/29.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
