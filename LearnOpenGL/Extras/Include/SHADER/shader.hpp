0c2d06916db3abc548174dd56f8ddfaf9a8bb050
scong-Liu <scongliu777@gmail.com>
2017-10-30 00:25:53 +0800

    Package Shader

---
+++ LearnOpenGL/Extras/Include/SHADER/shader.hpp
@@ -0,0 +1,15 @@
+//
+//  shader.hpp
+//  LearnOpenGL
+//
+//  Created by Apple on 2017/10/29.
+//  Copyright © 2017年 Apple. All rights reserved.
+//
+
+#ifndef shader_hpp
+#define shader_hpp
+
+
+
+#endif /* shader_hpp */
+
--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -7,13 +7,35 @@
 //
 
 #include <iostream>
-#include <glad.h>
-#include <glfw3.h>
+#include <cmath>
 
-using namespace std;
+#include <GLAD/glad.h>
+#include <GLFW/glfw3.h>
 
 void framebuffer_size_callback(GLFWwindow* window,int width,int height);
 void processInput(GLFWwindow *window);
+
+// settings
+using namespace std;
+const unsigned int SCR_WIDTH=800;
+const unsigned int SCR_HEIGHT=600;
+
+const char *vertexShaderSource="#version 330 core\n"
+    "layout (location=0) in vec3 aPos;\n"
+    "layout (location=1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "gl_Position=vec4(aPos,1.0);\n"
+    "ourColor=aColor;"
+    "}\0";
+const char *fragmentShaderSource="#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor=vec4(ourColor,1.0);\n"
+    "}\n\0";
 
 int main(int argc,const char * argv[]){
     //init GLWF
@@ -31,6 +53,7 @@ int main(int argc,const char * argv[]){
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
     
     //init GLAD
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
@@ -38,9 +61,30 @@ int main(int argc,const char * argv[]){
         return -1;
     }
     
-    //Viewport Dimension
-    glViewport(0,0,800,600);
-    glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
+    
+    //set up vertex data (and buffer(s)) and configure vertex attributes
+    float vertices[]={
+        0.5f,-0.5f,0.0f,  1.0f,0.0f,0.0f,
+        -0.5f,-0.5f,0.0f, 0.0f,1.0f,0.0f,
+        0.0f,0.5f,0.0f,   0.0f,0.0f,1.0f
+    };
+    
+    //Vertex Array Object,Vertex Buffer Object
+    unsigned int VBO,VAO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1,&VBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3* sizeof(float)));
+    glEnableVertexAttribArray(1);
+    //glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(VAO);
     
     //Render Loop
     while(!glfwWindowShouldClose(window)){
@@ -49,11 +93,20 @@ int main(int argc,const char * argv[]){
         glClearColor(0.2f,0.3f,0.3f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        glUseProgram(shaderProgram);
+        float timeValue=glfwGetTime();
+        float greenValue=sin(timeValue)/2.0f+0.5f;
+        int vertexColorLocation=glGetUniformLocation(shaderProgram,"ourColor");
+        glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
+        glDrawArrays(GL_TRIANGLES,0,3);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
     
     //release resources
+    glDeleteVertexArrays(1,&VAO);
+    glDeleteBuffers(1,&VBO);
     glfwTerminate();
     return 0;
 }
