3a5a7ec67cb8eead81d67915db61c73505c36391
scong-Liu <scongliu777@gmail.com>
2017-10-31 20:43:37 +0800

    10.31

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -14,7 +14,7 @@
 #include <SHADER/shader.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window,int width,int height);
-void processInput(GLFWwindow *window);
+void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods);
 
 // settings
 using namespace std;
@@ -30,13 +30,14 @@ int main(int argc,const char * argv[]){
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,GL_TRUE);
     
     //create window object
-    GLFWwindow* window=glfwCreateWindow(800,600,"LearnOpenGL",NULL,NULL);
+    GLFWwindow* window=glfwCreateWindow(SCR_WIDTH,SCR_HEIGHT,"LearnOpenGL",NULL,NULL);
     if (window==NULL){
         cout<<"Failed to create GLFW window"<<endl;
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window,key_callback);
     glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
     
     //init GLAD
@@ -44,16 +45,16 @@ int main(int argc,const char * argv[]){
         cout<<"Failed to initialize GLAD"<<endl;
         return -1;
     }
-    
     glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    
     Shader ourShader("/Users/apple/Documents/OpenGL/LearnOpenGL/LearnOpenGL/Extras/Include/SHADER/VertexShader.glsl",
                      "/Users/apple/Documents/OpenGL/LearnOpenGL/LearnOpenGL/Extras/Include/SHADER/FragmentShader.glsl");
     
     //set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[]={
-        0.5f,-0.5f,0.0f,  1.0f,0.0f,0.0f,
-        -0.5f,-0.5f,0.0f, 0.0f,1.0f,0.0f,
-        0.0f,0.5f,0.0f,   0.0f,0.0f,1.0f
+        0,0,0,      1.0f,0.0f,0.0f,
+        0.5f,0.5f,0,      0.0f,1.0f,0.0f,
+        -0.5,-0.5,0,    0.0f,0.0f,1.0f
     };
     
     //Vertex Array Object,Vertex Buffer Object
@@ -66,25 +67,20 @@ int main(int argc,const char * argv[]){
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3* sizeof(float)));
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
-    //glBindBuffer(GL_ARRAY_BUFFER,0);
-    //glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(VAO);
     
     //Render Loop
     while(!glfwWindowShouldClose(window)){
-        processInput(window);
-        
-        glClearColor(0.2f,0.3f,0.3f,1.0f);
+        glClearColor(0.0f,0.0f,0.0f,0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
         ourShader.use();
-        glBindVertexArray(VAO);
-        /*float timeValue=glfwGetTime();
-        float greenValue=sin(timeValue)/2.0f+0.5f;
-        int vertexColorLocation=glGetUniformLocation(shaderProgram,"ourColor");
-        glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);*/
-        glDrawArrays(GL_TRIANGLES,0,3);
+        //glBindVertexArray(VAO);
+        glPointSize(20);
+        glDrawArrays(GL_LINES,0,3);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -93,6 +89,7 @@ int main(int argc,const char * argv[]){
     //release resources
     glDeleteVertexArrays(1,&VAO);
     glDeleteBuffers(1,&VBO);
+    glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
 }
@@ -101,8 +98,8 @@ void framebuffer_size_callback(GLFWwindow* window,int width,int height){
     glViewport(0,0,width,height);
 }
 
-void processInput(GLFWwindow *window){
-    if(glfwGetKey(window,GLFW_KEY_ESCAPE)==GLFW_PRESS)
-        glfwSetWindowShouldClose(window,true);
+void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods){
+    if((key==GLFW_KEY_ESCAPE)&&(action==GLFW_PRESS))
+        glfwSetWindowShouldClose(window,GLFW_TRUE);
 }
 
--- LearnOpenGL/LearnOpenGL/shader.cpp
+++ LearnOpenGL/LearnOpenGL/shader.cpp
@@ -7,154 +7,143 @@
 //
 
 #include "SHADER/shader.hpp"
-#include <GLAD/glad.h>
-
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-
 using namespace std;
 
-class Shader{
-public:
-    unsigned int ID;
-    Shader(const char* vertexPath,const char* fragmentPath,const char* geometryPath=nullptr){
-        string vertexCode;
-        string fragmentCode;
-        string geometryCode;
-        ifstream vShaderFile;
-        ifstream fShaderFile;
-        ifstream gShaderFile;
-        vShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
-        fShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
-        gShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
-        try{
-            vShaderFile.open(vertexPath);
-            fShaderFile.open(fragmentPath);
-            stringstream vShaderStream,fShaderStream,gShaderStream;
-            vShaderStream<<vShaderFile.rdbuf();
-            fShaderStream<<fShaderFile.rdbuf();
-            vShaderFile.close();
-            fShaderFile.close();
-            vertexCode=vShaderStream.str();
-            fragmentCode=fShaderStream.str();
-            if(geometryPath!=nullptr){
-                gShaderFile.open(geometryPath);
-                stringstream gShaderStream;
-                gShaderStream<<gShaderFile.rdbuf();
-                gShaderFile.close();
-                geometryCode=gShaderStream.str();
-            }
-        }
-        catch(ifstream::failure e){
-            cout<<"ERROR:SHADER:FILE_NOT_SUCCESSFULLY_READ"<<endl;
-        }
-        const char* vShaderCode=vertexCode.c_str();
-        const char* fShaderCode=fragmentCode.c_str();
-        unsigned int vertex,fragment;
-        //vertex shader
-        vertex=glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertex,1,&vShaderCode,NULL);
-        glCompileShader(vertex);
-        checkCompileErrors(vertex,"VERTEX");
-        //fragment shader
-        fragment=glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragment,1,&fShaderCode,NULL);
-        glCompileShader(fragment);
-        checkCompileErrors(fragment,"FRAGMENT");
-        //geometry shader
-        unsigned int geometry;
+Shader::Shader(const char* vertexPath,const char* fragmentPath,const char* geometryPath){
+    string vertexCode;
+    string fragmentCode;
+    string geometryCode;
+    ifstream vShaderFile;
+    ifstream fShaderFile;
+    ifstream gShaderFile;
+    vShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
+    fShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
+    gShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
+    try{
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        stringstream vShaderStream,fShaderStream,gShaderStream;
+        vShaderStream<<vShaderFile.rdbuf();
+        fShaderStream<<fShaderFile.rdbuf();
+        vShaderFile.close();
+        fShaderFile.close();
+        vertexCode=vShaderStream.str();
+        fragmentCode=fShaderStream.str();
         if(geometryPath!=nullptr){
-            const char* gShaderCode=geometryCode.c_str();
-            geometry=glCreateShader(GL_GEOMETRY_SHADER);
-            glShaderSource(geometry,1,&gShaderCode,NULL);
-            glCompileShader(geometry);
-            checkCompileErrors(geometry,"GEOMETRY");
-        }
-        //shader program
-        ID=glCreateProgram();
-        glAttachShader(ID,vertex);
-        glAttachShader(ID,fragment);
-        if(geometryPath!=nullptr)
-            glAttachShader(ID,geometry);
-        glLinkProgram(ID);
-        checkCompileErrors(ID,"PROGRAM");
-        
-        glDeleteShader(vertex);
-        glDeleteShader(fragment);
-        if(geometryPath!=nullptr)
-            glDeleteShader(geometry);
-    }
-    
-    void use(){
-        glUseProgram(ID);
-    }
-    
-    void setBool(const string &name,bool value) const{
-        glUniform1i(glGetUniformLocation(ID,name.c_str()),(int)value);
-    }
-    
-    void setInt(const string &name,int value) const{
-        glUniform1i(glGetUniformLocation(ID,name.c_str()),value);
-    }
-    
-    void setFloat(const string &name,float value) const{
-        glUniform1f(glGetUniformLocation(ID,name.c_str()),value);
-    }
-    
-    void setVec2(const string &name,const glm::vec2 &value) const{
-        glUniform2f(glGetUniformLocation(ID,name.c_str()),1,&value[0]);
-    }
-    
-    void setVec2(const string &name,float x,float y) const{
-        glUniform2f(glGetUniformLocation(ID,name.c_str()),x,y);
-    }
-    
-    void setVec3(const string &name,const glm::vec3 &value) const{
-        glUniform3fv(glGetUniformLocation(ID,name.c_str()),1,&value[0]);
-    }
-    
-    void setVec3(const string &name,float x,float y,float z) const{
-        glUniform3f(glGetUniformLocation(ID,name.c_str()),x,y,z);
-    }
-    
-    void setVec4(const string &name,const glm::vec4 &value) const{
-        glUniform4fv(glGetUniformLocation(ID,name.c_str()),1,&value[0]);
-    }
-    
-    void setVec4(const string &name,float x,float y,float z,float w){
-        glUniform4f(glGetUniformLocation(ID,name.c_str()),x,y,z,w);
-    }
-    
-    void setMat2(const string &name,const glm::mat2 &mat) const{
-        glUniformMatrix2fv(glGetUniformLocation(ID,name.c_str()),1,GL_FALSE,&mat[0][0]);
-    }
-    
-    void setMat3(const string &name,const glm::mat3 &mat) const{
-        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()),1,GL_FALSE,&mat[0][0]);
-    }
-    
-    void setMat4(const string &name,const glm::mat4 &mat) const{
-        glUniformMatrix4fv(glGetUniformLocation(ID,name.c_str()),1,GL_FALSE,&mat[0][0]);
-    }
-private:
-    void checkCompileErrors(GLuint shader,string type){
-        GLint success;
-        GLchar infoLog[1024];
-        if(type!="PROGRAM"){
-            glGetShaderiv(shader,GL_COMPILE_STATUS,&success);
-            if(!success){
-                glGetShaderInfoLog(shader,1024,NULL,infoLog);
-                cout<<"ERROR::SHADER_COMPILATION_FAILED of type: "<<type<<"\n"<<infoLog<<endl;
-            }
-        }
-        else{
-            glGetProgramiv(shader,GL_LINK_STATUS,&success);
-            if(!success){
-                glGetProgramInfoLog(shader,1024,NULL,infoLog);
-                cout<<"ERROR::PROGRAM_LINKING_ERROR of type: "<<type<<"\n"<<infoLog<<endl;
-            }
+            gShaderFile.open(geometryPath);
+            stringstream gShaderStream;
+            gShaderStream<<gShaderFile.rdbuf();
+            gShaderFile.close();
+            geometryCode=gShaderStream.str();
         }
     }
-};
+    catch(ifstream::failure e){
+        cout<<"ERROR:SHADER:FILE_NOT_SUCCESSFULLY_READ"<<endl;
+    }
+    const char* vShaderCode=vertexCode.c_str();
+    const char* fShaderCode=fragmentCode.c_str();
+    unsigned int vertex,fragment;
+    //vertex shader
+    vertex=glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex,1,&vShaderCode,NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex,"VERTEX");
+    //fragment shader
+    fragment=glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment,1,&fShaderCode,NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment,"FRAGMENT");
+    //geometry shader
+    unsigned int geometry;
+    if(geometryPath!=nullptr){
+        const char* gShaderCode=geometryCode.c_str();
+        geometry=glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geometry,1,&gShaderCode,NULL);
+        glCompileShader(geometry);
+        checkCompileErrors(geometry,"GEOMETRY");
+    }
+    //shader program
+    ID=glCreateProgram();
+    glAttachShader(ID,vertex);
+    glAttachShader(ID,fragment);
+    if(geometryPath!=nullptr)
+        glAttachShader(ID,geometry);
+    glLinkProgram(ID);
+    checkCompileErrors(ID,"PROGRAM");
+    
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+    if(geometryPath!=nullptr)
+        glDeleteShader(geometry);
+}
+
+void Shader::use(){
+    glUseProgram(ID);
+}
+
+void Shader::setBool(const string &name,bool value) const{
+    glUniform1i(glGetUniformLocation(ID,name.c_str()),(int)value);
+}
+
+void Shader::setInt(const string &name,int value) const{
+    glUniform1i(glGetUniformLocation(ID,name.c_str()),value);
+}
+
+void Shader::setFloat(const string &name,float value) const{
+    glUniform1f(glGetUniformLocation(ID,name.c_str()),value);
+}
+/*
+ void setVec2(const string &name,const glm::vec2 &value) const{
+ glUniform2f(glGetUniformLocation(ID,name.c_str()),1,&value[0]);
+ }
+ 
+ void setVec2(const string &name,float x,float y) const{
+ glUniform2f(glGetUniformLocation(ID,name.c_str()),x,y);
+ }
+ 
+ void setVec3(const string &name,const glm::vec3 &value) const{
+ glUniform3fv(glGetUniformLocation(ID,name.c_str()),1,&value[0]);
+ }
+ 
+ void setVec3(const string &name,float x,float y,float z) const{
+ glUniform3f(glGetUniformLocation(ID,name.c_str()),x,y,z);
+ }
+ 
+ void setVec4(const string &name,const glm::vec4 &value) const{
+ glUniform4fv(glGetUniformLocation(ID,name.c_str()),1,&value[0]);
+ }
+ 
+ void setVec4(const string &name,float x,float y,float z,float w) const{
+ glUniform4f(glGetUniformLocation(ID,name.c_str()),x,y,z,w);
+ }
+ 
+ void setMat2(const string &name,const glm::mat2 &mat) const{
+ glUniformMatrix2fv(glGetUniformLocation(ID,name.c_str()),1,GL_FALSE,&mat[0][0]);
+ }
+ 
+ void setMat3(const string &name,const glm::mat3 &mat) const{
+ glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()),1,GL_FALSE,&mat[0][0]);
+ }
+ 
+ void setMat4(const string &name,const glm::mat4 &mat) const{
+ glUniformMatrix4fv(glGetUniformLocation(ID,name.c_str()),1,GL_FALSE,&mat[0][0]);
+ }*/
+void Shader::checkCompileErrors(GLuint shader,string type){
+    GLint success;
+    GLchar infoLog[1024];
+    if(type!="PROGRAM"){
+        glGetShaderiv(shader,GL_COMPILE_STATUS,&success);
+        if(!success){
+            glGetShaderInfoLog(shader,1024,NULL,infoLog);
+            cout<<"ERROR::SHADER_COMPILATION_FAILED of type: "<<type<<"\n"<<infoLog<<endl;
+        }
+    }
+    else{
+        glGetProgramiv(shader,GL_LINK_STATUS,&success);
+        if(!success){
+            glGetProgramInfoLog(shader,1024,NULL,infoLog);
+            cout<<"ERROR::PROGRAM_LINKING_ERROR of type: "<<type<<"\n"<<infoLog<<endl;
+        }
+    }
+}
+
