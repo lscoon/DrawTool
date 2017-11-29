9289fd11aeb91c82a1bf2d32e394387e19a03e66
scong-Liu <scongliu777@gmail.com>
2017-11-29 22:19:56 +0800

    Something about camera

--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -19,11 +19,28 @@
 
 void framebuffer_size_callback(GLFWwindow* window,int width,int height);
 void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
 // settings
 using namespace std;
 const unsigned int SCR_WIDTH=800;
 const unsigned int SCR_HEIGHT=600;
+
+//camera
+glm::vec3 cameraPos = glm::vec3(0.0f,0.0f,3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f,0.0f,-1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f,1.0f,0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;
+float pitch = 0.0f;
+float lastX = float(SCR_WIDTH) / 2.0;
+float lastY = float(SCR_HEIGHT) / 2.0;
+float fov = 45.0f;
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 int main(int argc,const char * argv[]){
     //init GLWF
@@ -42,6 +59,8 @@ int main(int argc,const char * argv[]){
     }
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window,key_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
     glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
     
     //init GLAD
@@ -50,32 +69,78 @@ int main(int argc,const char * argv[]){
         return -1;
     }
     glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+    glEnable(GL_DEPTH_TEST);
     
     Shader ourShader("/Users/apple/Documents/OpenGL/LearnOpenGL/LearnOpenGL/Extras/Include/SHADER/VertexShader.glsl",
                      "/Users/apple/Documents/OpenGL/LearnOpenGL/LearnOpenGL/Extras/Include/SHADER/FragmentShader.glsl");
     
-    float vertices[]={
-        // position           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left
+    float vertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+        
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
     };
-    unsigned int indices[]={
-        0,1,3,
-        1,2,3
+    
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
     };
     
     unsigned int VBO,VAO,EBO;
     glGenVertexArrays(1,&VAO);
     glGenBuffers(1,&VBO);
-    glGenBuffers(1,&EBO);
+    //glGenBuffers(1,&EBO);
     
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER,VBO);
     glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(indices),indices,GL_STATIC_DRAW);
     
     glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,5*sizeof(float),(void*)0);
     glEnableVertexAttribArray(0);
@@ -87,7 +152,7 @@ int main(int argc,const char * argv[]){
     glGenTextures(1,&texture1);
     glBindTexture(GL_TEXTURE_2D,texture1);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
     int width,height,nrChannels;
@@ -103,7 +168,7 @@ int main(int argc,const char * argv[]){
     glGenTextures(1,&texture2);
     glBindTexture(GL_TEXTURE_2D,texture2);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
     data=stbi_load("/Users/apple/Documents/OpenGL/LearnOpenGL/LearnOpenGL/Extras/Resources/bear.jpg",&width,&height,&nrChannels,0);
@@ -118,29 +183,39 @@ int main(int argc,const char * argv[]){
     ourShader.setInt("texture1",0);
     ourShader.setInt("texture2",1);
     
+    
+    
     //Render Loop
     while(!glfwWindowShouldClose(window)){
-        glClearColor(0.0f,0.0f,0.0f,0.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        //bind texture
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        
+        glClearColor(0.2f,0.3f,0.3f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+        
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D,texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D,texture2);
         
         ourShader.use();
-        glm::mat4 model(1);
-        glm::mat4 view(1);
-        glm::mat4 projection(1);
-        model = glm::rotate(model,0.0f,glm::vec3(1.0f,0.0f,0.0f));
-        view = glm::translate(view,glm::vec3(0.0f, 0.0f, -0.5f));
-        //projection = glm::perspective(glm::radians(0.0f),(float)SCR_WIDTH/(float)SCR_HEIGHT,0.1f,0.4f);
-        ourShader.setMat4("model",model);
-        ourShader.setMat4("view",view);
+        glm::mat4 projection = glm::perspective(fov,(float)(SCR_WIDTH/SCR_HEIGHT),0.1f,100.0f);
         ourShader.setMat4("projection",projection);
+        glm::mat4 view = glm::lookAt(cameraPos,cameraPos+cameraFront,cameraUp);
+        ourShader.setMat4("view",view);
         
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES,6,GL_UNSIGNED_INT,0);
+        for(unsigned int i=0;i<10;i++){
+            glm::mat4 model(1);
+            model=glm::translate(model,cubePositions[i]);
+            //float angle=(float)i*100.0f;
+            //model = glm::rotate(model,angle,glm::vec3(1.0f,0.3f,0.5f));
+            model = glm::rotate(model, (float)(glfwGetTime()+i)*10, glm::vec3(0.5f, 1.0f, 0.0f));
+            ourShader.setMat4("model",model);
+        
+            glDrawArrays(GL_TRIANGLES,0,36);
+        }
         
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -149,7 +224,7 @@ int main(int argc,const char * argv[]){
     //release resources
     glDeleteVertexArrays(1,&VAO);
     glDeleteBuffers(1,&VBO);
-    glDeleteBuffers(1,&EBO);
+    //glDeleteBuffers(1,&EBO);
     
     glfwDestroyWindow(window);
     glfwTerminate();
@@ -161,8 +236,59 @@ void framebuffer_size_callback(GLFWwindow* window,int width,int height){
 }
 
 void key_callback(GLFWwindow* window,int key,int scancode,int action,int mods){
+    float cameraSpeed = 2.5f * deltaTime;
     if((key==GLFW_KEY_ESCAPE)&&(action==GLFW_PRESS))
         glfwSetWindowShouldClose(window,GLFW_TRUE);
+    else if((key==GLFW_KEY_W)&&(action==GLFW_PRESS))
+        cameraPos += cameraSpeed * cameraFront;
+    else if((key==GLFW_KEY_S)&&(action==GLFW_PRESS))
+        cameraPos -= cameraSpeed * cameraFront;
+    else if((key==GLFW_KEY_A)&&(action==GLFW_PRESS))
+        cameraPos -= glm::normalize(glm::cross(cameraFront,cameraUp)) * cameraSpeed;
+    else if((key==GLFW_KEY_D)&&(action==GLFW_PRESS))
+        cameraPos += glm::normalize(glm::cross(cameraFront,cameraUp)) * cameraSpeed;
 }
 
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+    
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+    
+    float sensitivity = 0.1f; // change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+    
+    yaw += xoffset;
+    pitch += yoffset;
+    
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+    
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
 
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    if (fov >= 1.0f && fov <= 45.0f)
+        fov -= yoffset;
+    if (fov <= 1.0f)
+        fov = 1.0f;
+    if (fov >= 45.0f)
+        fov = 45.0f;
+}
